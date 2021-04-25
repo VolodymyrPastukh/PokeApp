a53aec0c9b12504ac8d6198c513c62a0a73c6020
VolodymyrPastukh <saske7258@gmail.com>
2021-04-25 16:00:51 +0300

    Start of development (Title Screen, nav, logger)

--- app/src/main/java/com/vovan/pokeapp/MainActivity.kt
+++ app/src/main/java/com/vovan/pokeapp/MainActivity.kt
@@ -9,6 +9,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
 
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.navHost, TitleFragment())
+            .commit()
     }
 }
